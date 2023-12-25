7f66308332f8ad517cf5c78e7438b50302449ef5
Home <zavalnyy_saveliy@mail.ru>
2023-12-25 15:43:29 +1000

    complited!1-2

--- src/main/java/calculator/calculatordemo/controller/calculatorController.java
+++ src/main/java/calculator/calculatordemo/controller/calculatorController.java
@@ -1,4 +1,48 @@
 package calculator.calculatordemo.controller;
 
+import calculator.calculatordemo.CalculatordemoApplication;
+import calculator.calculatordemo.service.CalculatorService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import service.CalculatorService;
 
+@RestController
+public class CalculatorController {
+
+    private final CalculatorService service;
+
+    public CalculatorController(CalculatorService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/calculator")
+    public String hello() {
+        return "Калькулируйте на здоровье";
+    }
+
+    // /calculator/plus?num1=5&num2=5
+    @GetMapping("/calculator/plus")
+    public String plus(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " + " + num2 + " = " + service.plus(num1, num2);
+    }
+    @GetMapping("/calculator/minus")
+    public String minus(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
+        if (num1 == null || num2 == null) {
+            return "Один из аргументов не передан!";
+        }
+        return num1 + " + " + num2 + " = " + service.minus(num1, num2);
+    }
+    @GetMapping("/calculator/multiply")
+    public String multiply(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " * " + num2 + " = " + service.multiply(num1, num2);
+    }
+    @GetMapping("/calculator/divide")
+    public String division(@RequestParam int num1, @RequestParam int num2) {
+        if (num2 == 0) {
+            return "На 0 делить нельзя!";
+        }
+        return num1 + " / " + num2 + " = " + service.divide(num1, num2);
+    }
+}
 
